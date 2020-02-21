8d8d2da5979d0a62b779eeaa282524ee3687872c
KenwayTheWise <embangwicaksono@gmail.com>
2020-02-21 21:35:45 +0700

    fix add features

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ItemList from "../components/itemlist"
-import addItemForm from "../components/additemform"
+import AddItemForm from "../components/additemform"
 const IndexPage = () => {
   
   return (
@@ -9,7 +9,7 @@ const IndexPage = () => {
       <h2>Item List</h2>
       <ItemList/>
       <h2>Add Item</h2>
-      <addItemForm/>
+      <AddItemForm/>
     </div>
   )
 }
