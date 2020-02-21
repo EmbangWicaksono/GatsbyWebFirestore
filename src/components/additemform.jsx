b6e35947aad83fde625401d3e173d97639773a83
KenwayTheWise <embangwicaksono@gmail.com>
2020-02-21 21:11:05 +0700

    add item features

---
+++ src/components/additemform.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import firebase from 'firebase'
+
+const addItemForm = () => {
+    //useState() hook captures the value from the input value
+  const [name, setName] = useState("")
+  const [type, setType] = useState("")
+  const [qty, setQty] = useState(null)
+  const [description, setDescription] = useState("")
+
+  /* The onSubmit function we takes the 'e'
+    or event and submits it to Firebase
+    */
+  const onSubmit = e => {
+    /* 
+    preventDefault is important because it
+    prevents the whole page from reloading
+    */
+    e.preventDefault()
+    firebase
+      .firestore()
+      .collection("items")
+      .add({
+        name,
+        type,
+        qty,
+        description,
+      })
+      //.then will reset the form to nothing
+      .then(() => setName(""), setType(""), setQty(''), setDescription(""))
+  }
+    return (
+        <form onSubmit={onSubmit}>
+            <input type="text" name="name" placeholder="Name" value={name} onChange={e => setName(e.currentTarget.value)}/>
+            <input type="text" name="type" placeholder="Type" value={type} onChange={e => setType(e.currentTarget.value)}/>
+            <input type="number" name="qty" placeholder="qty" value={qty} onChange={e => setQty(e.currentTarget.value)}/>
+            <input type="text" name="description" placeholder="description" value={description} onChange={e => setDescription(e.currentTarget.value)}/>
+            <button>Submit</button>
+        </form>
+    )
+}
+
+export default addItemForm
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,15 @@
 import React from "react"
-// import firebase from "../components/firebase"
 import ItemList from "../components/itemlist"
-// import { Link } from "gatsby"
-
-// import Layout from "../components/layout"
-// import Image from "../components/image"
-// import SEO from "../components/seo"
-
+import addItemForm from "../components/additemform"
 const IndexPage = () => {
-  // firebase
-  //   .firestore()
-  //   .collection("items")
-  //   .add({
-  //     name: "Corona",
-  //     type: "beer",
-  //     qty: 5,
-  //     description:
-  //       "is a pale lager produced by Cervecería Modelo in Mexico",
-  //   })
-  //   .then(ref => {
-  //     console.log("Added document with ID: ", ref.id)
-  //   })
-
+  
   return (
     <div>
       <h1>Firestore CRUD App </h1>
       <h2>Item List</h2>
       <ItemList/>
       <h2>Add Item</h2>
+      <addItemForm/>
     </div>
   )
 }
